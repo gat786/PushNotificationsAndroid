e80513068a81b3be65147ed6159e50b7c94693c6
Ganesh Tiwari <ganesh@nethermind.io>
2022-02-04 19:30:53 +0530

    Added melange icons and button to copy token

--- app/src/main/java/io/nethermind/pushnotifications/MainActivity.kt
+++ app/src/main/java/io/nethermind/pushnotifications/MainActivity.kt
@@ -1,22 +1,33 @@
 package io.nethermind.pushnotifications
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.ktx.messaging
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity";
+    var firebaseToken = "";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getFirebaseToken()
+
+        copyTokenButton.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText(firebaseToken, firebaseToken);
+            clipboard.setPrimaryClip(clip)
+        }
     }
 
     fun getFirebaseToken() {
@@ -27,12 +38,13 @@ class MainActivity : AppCompatActivity() {
             }
 
             // Get new FCM registration token
-            val token = task.result
+            firebaseToken = task.result
 
             Firebase.messaging.subscribeToTopic("notifications");
 
             // Log and toast
-            val msg = token;
+            val msg = firebaseToken
+            deviceTokenTextView.text = firebaseToken
             Log.d(TAG, "Token is $msg")
             Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
         })
