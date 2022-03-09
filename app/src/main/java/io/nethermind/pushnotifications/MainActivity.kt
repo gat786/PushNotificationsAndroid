5e63f9c8f0a3d69ad52134f142fc2e6275f4b133
Ganesh Tiwari <ganesh@nethermind.io>
2022-03-09 19:20:58 +0530

    working on google one tap sign in

--- app/src/main/java/io/nethermind/pushnotifications/MainActivity.kt
+++ app/src/main/java/io/nethermind/pushnotifications/MainActivity.kt
@@ -1,13 +1,19 @@
 package io.nethermind.pushnotifications
 
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.amplifyframework.AmplifyException
-import com.amplifyframework.auth.AuthProvider
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
@@ -18,7 +24,13 @@ class MainActivity : AppCompatActivity() {
     companion object {
         val TAG = "MainActivity";
         var firebaseToken = "";
+
+        private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+        private var showOneTapUI = true
     }
+
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,19 +44,101 @@ class MainActivity : AppCompatActivity() {
 //            val intent = Intent(this, BarcodeScanner::class.java)
 //            startActivity(intent)
 
-            Amplify.Auth.signInWithSocialWebUI(AuthProvider.google(),
-                this,
-                {
-                    Log.d(TAG, "Signing in successful ${it.nextStep}")
-                    val currentUser = Amplify.Auth.currentUser;
-                    Log.d(TAG, "Current user is ${currentUser}")
-                },
-                {
-                    Log.d(TAG, "Signing in failed")
-                    it.printStackTrace()
-//                    Log.e(TAG, it.stackTrace.toString())
+//            Amplify.Auth.signInWithSocialWebUI(AuthProvider.google(),
+//
+//                this,
+//                {
+//                    Log.d(TAG, "Signing in successful ${it.nextStep}")
+//                    val currentUser = Amplify.Auth.currentUser;
+//                    Log.d(TAG, "Current user is ${currentUser}")
+//                },
+//                {
+//                    Log.d(TAG, "Signing in failed")
+//                    it.printStackTrace()
+////                    Log.e(TAG, it.stackTrace.toString())
+//                }
+//            )
+
+
+            oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener {
+                    try{
+                        startIntentSenderForResult(
+                            it.pendingIntent.intentSender, REQ_ONE_TAP,
+                            null, 0, 0, 0, null)
+                    }
+                    catch(e: IntentSender.SendIntentException){
+                        Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    }
+
                 }
-            )
+                .addOnFailureListener{
+                    Log.d(TAG, it.localizedMessage)
+                }
+        }
+
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId("653454831064-4e876fhhvk5ifbrr0nklmddgi4om715j.apps.googleusercontent.com")
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            ).setAutoSelectEnabled(true)
+            .build()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    val username = credential.id
+                    val password = credential.password
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with your backend.
+                            Log.d(TAG, "Got ID token.")
+                        }
+                        password != null -> {
+                            // Got a saved username and password. Use them to authenticate
+                            // with your backend.
+                            Log.d(TAG, "Got password.")
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token or password!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    // ...
+
+                    when (e.statusCode) {
+                        CommonStatusCodes.CANCELED -> {
+                            Log.d(TAG, "One-tap dialog was closed.")
+                            // Don't re-prompt the user.
+                            showOneTapUI = false
+                        }
+                        CommonStatusCodes.NETWORK_ERROR -> {
+                            Log.d(TAG, "One-tap encountered a network error.")
+                            // Try again or just ignore.
+                        }
+                        else -> {
+                            Log.d(TAG, "Couldn't get credential from result." +
+                                    " (${e.localizedMessage})")
+                        }
+                    }
+                }
+            }
         }
     }
 
